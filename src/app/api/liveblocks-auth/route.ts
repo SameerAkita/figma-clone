78615ca1f752310bea0989398045f29811fa82e5
Sameer <akita.sameer1@gmail.com>
2025-10-09 06:53:13 +0900

    liveblocks routes

--- src/app/api/liveblocks-auth/route.ts
+++ src/app/api/liveblocks-auth/route.ts
@@ -1,3 +1,26 @@
 import { Liveblocks } from "@liveblocks/node";
+import { auth } from "~/server/auth";
+import { db } from "~/server/db";
 
-const liveblocks = new Liveblocks({secret: "hello"})
+const liveblocks = new Liveblocks({secret: "hello"});
+
+export async function POST(req: Request) {
+    const userSession = await auth();
+
+    // get the users room, and invitations to rooms
+    const user = await db.user.findUniqueOrThrow({
+        where: { id: userSession?.user.id },
+    });
+
+    const session = liveblocks.prepareSession(user.id, {
+        userInfo: {
+            name: user.email ?? "Anonymous"
+        }
+    });
+
+    session.allow(`room:${"testing"}`, session.FULL_ACCESS);
+
+    const {status, body} = await session.authorize();
+
+    return new Response(body, { status });
+}
